0ea591ce434c2cb8ddf8574bbc4caffa3b607d6c
OlegBakulin <oleg_vampiriys@mail.ru>
2020-10-21 15:05:04 +0300

    Остановился на переделке ProductDetails

---
+++ WebStoreCoreApplication/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using WebStoreCoreApplication.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreCoreApplication.ViewModels
+{
+    public class ProductViewModel : INameEntity, IOrderEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
