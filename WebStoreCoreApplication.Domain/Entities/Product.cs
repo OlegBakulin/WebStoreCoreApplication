ccce475a45ba449ed9cec35673e0fe97f2fbdc60
OlegBakulin <oleg_vampiriys@mail.ru>
2020-10-22 01:23:28 +0300

    Добавка Продукт и ПродуктФильтр + Правки для рабочей версии

---
+++ WebStoreCoreApplication.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebStoreCoreApplication.Domain.Entities.Base;
+using WebStoreCoreApplication.Domain.Entities.Base.Interfaces;
+
+namespace WebStoreCoreApplication.Domain.Entities
+{
+    [Table("Product")]
+    public class Product : NameEntity, IOrderEntity
+    {
+        public int Order { get; set; }
+        public int CategoryId { get; set; }
+        public int? BrandId { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        //public string Manufacturer { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+
+        [ForeignKey("BrandId")]
+        public virtual Brand Brand { get; set; }
+
+    }
+}
