dc9d9855ed0621abb25ebc59b251fda410378207
OlegBakulin <oleg_vampiriys@mail.ru>
2020-10-25 14:28:07 +0300

    1:39:24 Сломалась студия, почти доделал урок, Добавил и подключил DbInitializer

--- WebStoreCoreApplication/Program.cs
+++ WebStoreCoreApplication/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebStoreCoreApplicatioc.DAL;
 
 namespace WebStoreCoreApplication
 {
@@ -13,7 +15,24 @@ namespace WebStoreCoreApplication
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    WebStoreContext context = services.GetRequiredService<WebStoreContext>();
+                    DbInitializer.Initializer(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "ERROR!!! FOR DB INITIALIZING!!!");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
